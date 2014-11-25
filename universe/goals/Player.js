cb41c5d60b8076537cbd019ae7b0bcb69b6efc28
Nicholas Frees <xanister@hotmail.com>
2014-11-25 03:37:23 +0000

    ...

--- universe/goals/Player.js
+++ universe/goals/Player.js
@@ -2,10 +2,17 @@ Goals.Player = {
     description: "Player",
     label: "Player Goal",
     getAction: function () {
-        if (this.clientConnection.inputs.A) {
-            return new Actions.Move({dir: -1});
-        } else if (this.clientConnection.inputs.D || this.clientConnection.inputs.touchCount > 0) {
+        var inputs = this.clientConnection.inputs;
+        var touches = inputs.touches || false;
+
+        if (touches) {
             return new Actions.Move({dir: 1});
+        } else {
+            if (inputs.A) {
+                return new Actions.Move({dir: -1});
+            } else if (this.clientConnection.inputs.D || this.clientConnection.inputs.touchCount > 0) {
+                return new Actions.Move({dir: 1});
+            }
         }
         return false;
     }
