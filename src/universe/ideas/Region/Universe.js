048733751357fca6ee339859f55425fad561b1d5
Nicholas Frees <nicholas_frees@condenast.com>
2015-05-09 14:57:13 -0400

    ...

--- src/universe/ideas/Region/Universe.js
+++ src/universe/ideas/Region/Universe.js
@@ -23,6 +23,24 @@ function Universe(slug) {
      */
     this.categories.push('universe');
 
+    /**
+     * Randomly populate the region
+     *
+     * @method populate
+     */
+    Universe.prototype.populate = function () {
+        Region.prototype.populate.call(this);
+
+        var goblin = new Goblin();
+        goblin.warp(500, 500, this);
+
+        // Move inner regions to match new buffer
+        for (var index in this.contents.region) {
+            var r = this.contents.region[index];
+            r.warp(r.x, this.innerHeight - this.buffer.bottom - (r.height / 2));
+        }
+    };
+
     /*
      * Universe defaults
      */
